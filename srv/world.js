b533afde5d71440aedf23535774a4bb6d2d961e0
Kamil Biegała <kamil.biegala@capgemini.com>
2024-01-11 17:09:14 +0100

    Deleted issues

--- srv/world.js
+++ srv/world.js
@@ -1,34 +1,7 @@
-const { log } = require('console')
-
 module.exports = class say {
   hello(req) {
     let {to} = req.data
-    const regex = /[\[]/;  // Noncompliant: '[' does not need to be escaped when inside a character class '[]'
-    const octal = '\8';    // Noncompliant: '8' is not valid octal number
-    const hello = 'Hello, world\!';    // Noncompliant: '!' is not a special character
-    const path  = `\/${some}\/${dir}`; // Noncompliant: '/' is not a special character
-    if (parameter === void 42) { // Noncompliant
-      // ...
-      let a = 3;
-      let x;
-      if (!(a === 2)) {
-        alert("asd")
-      }
-
-      if (x == 0) {
-        doSomething();
-      } else if (x == 1) {
-        doSomethingElse();
-      }
-
-      let j = 0;
-      while (true) { // Noncompliant: constant end condition
-        j++;
-      }
-      
-   }
     if (to === 'me') to = require('os').userInfo().username
     return `Hello ${to}! he he2`
-    log("asd")
   }
 }
